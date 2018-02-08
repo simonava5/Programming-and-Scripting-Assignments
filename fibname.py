3f3023a97fdc0f0ca628cc5dfb91217b984b95b5
Simona <35840608+simonava5@users.noreply.github.com>
2018-02-08 17:28:41 +0000

    Update fibname.py

--- fibname.py
+++ fibname.py
@@ -26,3 +26,20 @@ print("My surname is", name)
 print("The first letter", first, "is number", firstno)
 print("The last letter", last, "is number", lastno)
 print("Fibonacci number", x, "is", ans)
+
+# Following text is the output for exercise 2 above
+
+# My surname is Vasiliauskaite
+# The first letter V is number 86
+# The last letter e is number 101
+# Fibonacci number 187 is 538522340430300790495419781092981030533
+
+---
+# What does ord() mean? My answer is below
+# Like a lot of the students here I didn't know what ord() command meant until I looked it up on Google.
+# It makes sense now that all numbers, letters, characters etc have numeric codes and when I looked up the ASCII character set,
+# the capital V and lower case e matched the output in visual code.
+
+
+# Exercise 1 - My name is Simona, so the first and last letter of my name (S + A = 19 + 1) give the number  20.
+# The 20th Fibonacci number is 6765. 
