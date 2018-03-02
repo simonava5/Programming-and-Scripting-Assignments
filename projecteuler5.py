c213e777cc141d1ac48325120fa7b3864134e465
Simona <35840608+simonava5@users.noreply.github.com>
2018-03-02 17:00:27 +0000

    Create projecteuler5.py

---
+++ projecteuler5.py
@@ -0,0 +1,17 @@
+# Simona Vasiliauskaite
+# 02/03/2018
+# Project Euler 5
+
+# Exercise
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+i = 1
+for x in (range(1, 21)): # all the numbers from 1 to 21 not including 21
+  if i % x > 0: 
+    for n in range(1, 21): 
+      if (i*n) % x == 0: 
+        i *= n 
+        break 
+
+print(i)
