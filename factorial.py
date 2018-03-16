1508cc16c6a6fa18ce64a02081024be68d219d1a
Simona <35840608+simonava5@users.noreply.github.com>
2018-03-16 11:09:48 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -8,5 +8,9 @@ def factorial(n):
         return 1 # if it is, 0 equals to 1
     else:
         return n * factorial(n-1) 
-n=int(input("Input a number to calculate the factorial : "))
+n=int(input("Input a number to calculate the factorial : ")) 
 print(factorial(n))
+
+# the factorial of a number 5 is 120
+# the factorial of a number 7 is 5040
+# the factorial of a number 10 is 3628800
