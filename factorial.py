bd6d8bcd8ec979327bd01e465f97cf6732520656
Simona <35840608+simonava5@users.noreply.github.com>
2018-03-16 11:05:07 +0000

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,11 @@
+# Simona Vasiliauskaite
+# Exercise 6 - Calculating the factorial of a number
+
+
+def factorial(n):
+    if n == 0: # test that the factorial number isn't 0
+        return 1 # if it is, 0 equals to 1
+    else:
+        return n * factorial(n-1) 
+n=int(input("Input a number to calculate the factorial : "))
+print(factorial(n))
