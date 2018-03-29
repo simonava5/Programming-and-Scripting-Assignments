f4581d6550b9bc5e53811542cd9f46ae96e8f0e9
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-03-29 20:31:24 +0100

    Added comments on line 5, 6, 7 and 14

--- factorial.py
+++ factorial.py
@@ -2,12 +2,16 @@
 # 16/03/2018
 # Exercise 6 - Calculating the factorial of a number
 
+# Definition - Factorial of a number is 
+# calculated by multiplying it with all the numbers 
+# below it starting from 1.
+
 
 def factorial(n):
     if n == 0: # test that the factorial number isn't 0
         return 1 # if it is, 0 equals to 1
     else:
-        return n * factorial(n-1) 
+        return n * factorial(n-1) # multiply the number with all the numbers below it start from 1.
 n=int(input("Input a number to calculate the factorial : ")) 
 print(factorial(n))
 
