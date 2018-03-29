b3f6d62b4eaedd6159c3232cd2524a5acec32174
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-03-29 20:41:54 +0100

    Added description of data set on line 5 and 6

--- irisdata.py
+++ irisdata.py
@@ -2,6 +2,9 @@
 # 02/03/2018
 # Exercise 5 - Formatting Iris Data Set
 
+# The data set consists of 50 samples from each of three
+# species of Iris (Iris setosa, Iris virginica and Iris versicolor)
+
 with open('data/iris.csv') as f: #opens the file iris csv which is saved on my device
     for line in f: #loops through each line
         n = line.split(',') #splits each line into a list
