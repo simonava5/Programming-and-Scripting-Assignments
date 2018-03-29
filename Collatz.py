e07bc1e17b72827ccb6fdddb84ad98cd59a122ea
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-03-29 20:06:20 +0100

    Added definition of Collatz Conjecture

--- Collatz.py
+++ Collatz.py
@@ -1,6 +1,15 @@
 # Simona Vasiliauskaite 12/02/2018
 # Testing Collatz Conjecture
 
+
+# Definition: The Collatz conjecture is a conjecture 
+# in mathematics that concerns a sequence defined
+# as follows: start with any positive integer n.
+# Then each term is obtained from the previous term
+# as follows: if the previous term is even, the next 
+# term is one half the previous term.
+
+
 def collatz_sequence(n):
     num_seq = [n]
     if n < 1:
@@ -9,8 +18,7 @@ def collatz_sequence(n):
        if n % 2 == 0: 
          n = int(n / 2) # The formula states that if the number is even, divide it by two
        else:
-         n = int(3 * n + 1) # and if the number is odd, triple it and add one
-       num_seq.append(n)    
+         n = int(3 * n + 1) # and if the number is odd, triple it and add one    
     return num_seq
 
 print(collatz_sequence(17))
