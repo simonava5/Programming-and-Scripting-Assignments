ee932befcbff145cb7d44f54ebe94c7ec01e7885
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-03-29 20:11:19 +0100

    Added definition of Collatz Conjecture

--- Collatz.py
+++ Collatz.py
@@ -13,12 +13,13 @@
 def collatz_sequence(n):
     num_seq = [n]
     if n < 1:
-       return []
+       return [] # return an empty list
     while n > 1:
        if n % 2 == 0: 
-         n = int(n / 2) # The formula states that if the number is even, divide it by two
+         n = int(n / 2) # The formula states that if the number is even, divide integer by two
        else:
-         n = int(3 * n + 1) # and if the number is odd, triple it and add one    
+         n = int(3 * n + 1) # and if the number is odd, triple it and add one
+       num_seq.append(n)    
     return num_seq
 
 print(collatz_sequence(17))
