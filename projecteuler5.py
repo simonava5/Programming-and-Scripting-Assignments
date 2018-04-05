f5f3745def3401c1cf38b3d0837405bdfab191bd
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-04-05 20:26:41 +0100

    added comment on line 11

--- projecteuler5.py
+++ projecteuler5.py
@@ -8,7 +8,7 @@
 
 i = 1
 for x in (range(2, 21)): # all the numbers from 2 to 20
-  if i % x > 0: 
+  if i % x > 0: # test if the remainder of i is more than 0
     for n in range(2, 21): 
       if (i*n) % x == 0: 
         i *= n 
